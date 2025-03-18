a66daae225e37fe505fdf3926eb39a07d321dc5e
shaffay-aio <muhammad.shaffay@aioapp.com>
2025-03-18 14:55:21 +0500

    vlm separated from pipeline

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,24 @@
-from backend.processing import encode_image, json_validator, convert_to_dataframe
 from backend.model import prompt_template, model
+from backend.processing import encode_image, json_validator, convert_to_dataframe
 
-def vlm(image_stream, file_stream):
+def vlm(image_stream):
 
-    image_url = f"data:image/jpeg;base64, {encode_image(image_stream)}"
+    # process image
+    encoded   = encode_image(image_stream)
+    image_url = f"data:image/jpeg;base64, {encoded}"
     
+    # build prompt and perform inference
     prompt    = prompt_template(image_url)
     response  = model(prompt)
 
+    # process output
     parsed    = json_validator(response)
     dataframe = convert_to_dataframe(parsed)
 
     return dataframe
 
+def pipeline(image_stream, file_stream):
+    
+    dataframe = vlm(image_stream)
+
+    pass
