2934f77a95a289688ebe7253cc19a406fb7548f8
shaffay-aio <muhammad.shaffay@aioapp.com>
2025-03-18 13:02:14 +0500

    input file streams added

--- app.py
+++ app.py
@@ -5,9 +5,11 @@
 # returns final csv
 
 import uvicorn
-from fastapi import FastAPI
+from io import BytesIO
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-
+#from backend.main import vlm
+    
 import warnings 
 warnings.filterwarnings("ignore")
 
@@ -16,9 +18,20 @@ app = FastAPI()
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"],)
 
+# TODO: make it multi jpg uploader
 @app.post("/supermenu")
-def ocr():
-    pass 
+async def OCR(image: UploadFile = File(...), file: UploadFile = File(...)):
+
+    # Read and process the image
+    image_bytes = await image.read()
+    image_stream = BytesIO(image_bytes)
+    
+    # Read the excel file
+    file_data = await file.read()
+    file_stream = BytesIO(file_data)
+
+    #vlm()
+    return {"excel_file_size": len(file_data)} 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8041)
+    uvicorn.run(app, host="0.0.0.0", port=8042)
