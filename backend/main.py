8321d059157fb6d65c7ed008c2624005cc247981
shaffay-aio <muhammad.shaffay@aioapp.com>
2025-03-20 16:24:02 +0500

    debugging embedding module

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
+from backend.embeddings import merge
 from backend.model import prompt_template, model
 from backend.processing import encode_image, json_validator, convert_to_dataframe
 
-import os
-import openai
-import numpy as np
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def vlm(image_stream):
 
@@ -22,47 +22,14 @@ def vlm(image_stream):
     dataframe = convert_to_dataframe(parsed)
 
     return dataframe
-
-def generate_embeddings(text_list):
-
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-
-    response = openai.Embedding.create(
-        input=text_list,
-        model="text-embedding-ada-002"
-    )
-    embeddings = [embedding['embedding'] for embedding in response['data']]
-    return np.array(embeddings)
-
-def get_unmatched(df_embeddings, similarities, threshold=0.85):
-
-    unmatched_items = []
-
-    for i, row in df_embeddings.iterrows():
-        max_similarity = np.max(similarities[i])
-
-        if max_similarity < threshold:
-            unmatched_items.append(row)
-
-    return unmatched_items
             
-def merge(dataframe, file_stream):
-
-    # get embeddings
-    df_embeddings = generate_embeddings(dataframe['Item Name'].tolist())
-    fs_embeddings = generate_embeddings(file_stream['Item Name'].tolist())
-
-    # extract unfamiliar items
-    similarities  = cosine_similarity(df_embeddings, fs_embeddings)
-    unmatched     = get_unmatched(df_embeddings, similarities)
-
-    # add to online file 
-    unmatched_df  = pd.concat([file_stream, unmatched], ignore_index=True)
-    return unmatched_df
 
 def pipeline(image_stream, file_stream):
     
-    dataframe = vlm(image_stream)
-    merged    = merge(dataframe, file_stream)
+    #dataframe = vlm(image_stream)
+    print("menu extracted")
 
+    file = pd.read_excel(file_stream, sheet_name=None)
+    merged    = merge(file['items'], file['items'])
+    print("files marged")
     pass
