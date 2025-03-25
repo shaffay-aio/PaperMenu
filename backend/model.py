f7ba34eccd9717ebdb7839173934d9066d9278e1
shaffay-aio <muhammad.shaffay@aioapp.com>
2025-03-25 12:26:20 +0500

    system prompt updated, gemini model added

--- backend/model.py
+++ backend/model.py
@@ -1,15 +1,21 @@
 import os
 from openai import OpenAI
 
+sys_prompt = """You have to extract all the data from the menu in the JSON format.
+Do not write anything from yourself. If any information in below given format is missing keep it empty.
+Do not break the output structure. You have to extract only food categories, items, prices and descriptions.
+
+Output Format:
+[
+    {'category' : '', 'item': '', 'price': , 'description': ''}, 
+]
+"""
+
 def prompt_template(image_url):
 
     # output format json
     # necessary fields in output
     # any additional modifier/option fields
-    sys_prompt = """
-    You have to extract all the data from the menu in the JSON format.
-    Donot write anything from yourself.
-    """
 
     message = [
         {
@@ -17,19 +23,29 @@ def prompt_template(image_url):
             "content": 
             [
                 { "type": "text", "text": sys_prompt},
-                { "type": "image_url", "image_url": image_url},
+                { "type": "image_url", "image_url": {"url" : image_url}},
             ],
         },
     ]
 
     return message
 
-def model(prompt):
+def modelQwen(prompt):
     
     client = OpenAI(api_key=os.getenv("QWEN_API_KEY"), base_url="https://dashscope.aliyuncs.com/compatible-mode/v1")
 
     response = client.chat.completions.create(
     model = "qwen-vl-max", response_format = {'type':'json_object'}, messages=prompt
+    )
+
+    return response.choices[0].message.content
+
+def modelGemini(prompt):
+
+    client = OpenAI(api_key=os.getenv("GEMINI_API_KEY"), base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
+
+    response = client.chat.completions.create(
+    model = "gemini-2.0-flash-lite", response_format = {'type':'json_object'}, messages=prompt
     )
 
     return response.choices[0].message.content
