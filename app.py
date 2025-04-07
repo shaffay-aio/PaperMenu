ab65d264cf468a7268c200f2ea891954047a4cd0
shaffay-aio <muhammad.shaffay@aioapp.com>
2025-04-07 15:41:27 +0500

    multi file input enabled

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import io
 import uvicorn
 import pandas as pd
 from io import BytesIO
+from typing import List
 from fastapi import HTTPException
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
-from backend.main import pipeline, vlm
+from backend.main import pipeline, vlm, multiimage
 
 from backend.utils.logging import setup_logger
 logger = setup_logger(__name__)
@@ -19,21 +20,40 @@ app = FastAPI()
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"],)
 
+async def read_images(images: List[UploadFile]):
+
+    # TODO: image should be jpg error
+    image_streams = []
+    
+    # Read all images into byte streams first
+    for image in images:
+
+        try:
+            image_bytes = await image.read()
+            image_stream = BytesIO(image_bytes)
+            image_streams.append(image_stream)
+        except HTTPException as e:
+            raise HTTPException(status_code=e.status_code, detail=e.detail)
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Internal server error while reading image {image.filename}. {e}")
+
+    return image_streams
+
 @app.post("/ocr")
-async def ocr_only(image: UploadFile = File(...)):
+async def ocr_only(images: List[UploadFile] = File(...)):
 
     # Read and process the image
-    image_bytes = await image.read()
-    image_stream = BytesIO(image_bytes)
+    image_streams = await read_images(images)
 
     try:
-        response = vlm(image_stream)
+        response = multiimage(image_streams)
         
     except HTTPException as e:
         raise HTTPException(status_code=e.status_code, detail=e.detail)
     
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal server error. {e}")
+        raise HTTPException(status_code=500, detail=f"Internal server error. {e}")
 
     # convert excel file to byte stream object
     output = io.BytesIO() 
